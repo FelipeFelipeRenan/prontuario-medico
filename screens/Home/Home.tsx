afb2beaeb793713ad41d34704f2b0cce6c0b0f41
CC0043 - PROGRAMAÇAO PARA DISPOSITIVOS MOVEIS <mobile@espanha.k05s>
2023-04-05 13:36:14 -0300

    [DEV] Adding first styles to Home and Login screens

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  footer: {
+    marginBottom: 0,
+  },
+});
 
 export default function Home({navigation}: any): JSX.Element {
   return (
-    <View>
-      <Text>Logo do app</Text>
-      <Text>Nome do app</Text>
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <Text>Logo do app</Text>
+        <Text>Nome do app</Text>
+      </View>
       <Button
         title="Acessar o sistema"
         onPress={() => navigation.navigate('Login')}
       />
-      <Text>Copyright 2023 - equipe</Text>
+      <View style={styles.footer}>
+        <Text>Copyright 2023 - equipe</Text>
+      </View>
     </View>
   );
 }
--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -1,62 +1,82 @@
 import React, {useState} from 'react';
-import {Button, Text, TextInput, View} from 'react-native';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
 import DropdownSelect from 'react-native-input-select';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+  },
+});
 
 export default function Login({navigation}: any): JSX.Element {
   const [senha, setSenha] = useState('');
-  const [user, setUser] = useState('');
+  const [user, setUsuario] = useState('');
   return (
     <View>
-      <DropdownSelect
-        label="Tipo de usuario"
-        placeholder="Selecione o seu tipo de usuario"
-        options={[
-          {name: 'Médico', code: 'ME'},
-          {name: 'Enfermeiro', code: 'EN'},
-          {name: 'Paciente', code: 'PA'},
-        ]}
-        optionLabel={'name'}
-        optionValue={'code'}
-        selectedValue={user}
-        onValueChange={(value: React.SetStateAction<string>) => setUser(value)}
-        primaryColor={'green'}
-        // eslint-disable-next-line react-native/no-inline-styles
-        labelStyle={{
-          color: 'teal',
-          fontSize: 15,
-          fontWeight: '500',
-          alignSelf: 'center',
-          marginTop: '20%',
-        }}
-        // eslint-disable-next-line react-native/no-inline-styles
-        dropdownContainerStyle={{
-          borderColor: 'red',
-          alignContent: 'center',
-        }}
-        modalBackgroundStyle={{backgroundColor: 'rgba(196, 198, 246, 0.5)'}}
-        checkboxStyle={{backgroundColor: 'red', borderRadius: 5}}
-      />
-      <Text>{user}</Text>
-      <TextInput
-        placeholder="Senha"
-        value={senha}
-        secureTextEntry
-        onChangeText={value => {
-          setSenha(value);
-        }}
-      />
-      <Button
-        title="Entrar"
-        onPress={() => {
-          if (user === 'ME') {
-            navigation.navigate('Medico');
-          } else if (user === 'EN') {
-            navigation.navigate('Enfermeiro');
-          } else if (user === 'PA') {
-            navigation.navigate('Paciente');
+      <View style={styles.container}>
+        <DropdownSelect
+          label="Tipo de usuario"
+          placeholder="Selecione o seu tipo de usuario"
+          options={[
+            {name: 'Médico', code: 'ME'},
+            {name: 'Enfermeiro', code: 'EN'},
+            {name: 'Paciente', code: 'PA'},
+          ]}
+          optionLabel={'name'}
+          optionValue={'code'}
+          selectedValue={user}
+          onValueChange={(value: React.SetStateAction<string>) =>
+            setUsuario(value)
           }
-        }}
-      />
+          primaryColor={'green'}
+          // eslint-disable-next-line react-native/no-inline-styles
+          labelStyle={{
+            color: 'teal',
+            fontSize: 15,
+            fontWeight: '500',
+            alignSelf: 'center',
+          }}
+          // eslint-disable-next-line react-native/no-inline-styles
+          dropdownContainerStyle={{
+            borderColor: 'red',
+            alignContent: 'center',
+          }}
+          // eslint-disable-next-line react-native/no-inline-styles
+          modalBackgroundStyle={{backgroundColor: 'rgba(196, 198, 246, 0.5)'}}
+          // eslint-disable-next-line react-native/no-inline-styles
+          checkboxStyle={{backgroundColor: 'red', borderRadius: 5}}
+        />
+        <TextInput
+          placeholder="Usuário"
+          onChangeText={value => {
+            setUsuario(value);
+          }}
+        />
+        <TextInput
+          placeholder="Senha"
+          value={senha}
+          secureTextEntry
+          onChangeText={value => {
+            setSenha(value);
+          }}
+        />
+        <Button
+          title="Entrar"
+          onPress={() => {
+            if (user === 'ME') {
+              navigation.navigate('Medico');
+            } else if (user === 'EN') {
+              navigation.navigate('Enfermeiro');
+            } else if (user === 'PA') {
+              navigation.navigate('Paciente');
+            }
+          }}
+        />
+      </View>
     </View>
   );
 }
