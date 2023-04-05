fbee31682a83b0d889ae56db7cddecdf47f8063e
CC0043 - PROGRAMAÇAO PARA DISPOSITIVOS MOVEIS <mobile@espanha.k05s>
2023-04-05 11:04:20 -0300

    [DEV] Adding User screens and navigation

--- App.tsx
+++ App.tsx
@@ -3,6 +3,9 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from './screens/Home/Home';
 import Login from './screens/Login/Login';
+import Paciente from './screens/Users/Paciente';
+import Medico from './screens/Users/Medico';
+import Enfermeiro from './screens/Users/Enfermeiro';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +15,9 @@ function App(): JSX.Element {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Paciente" component={Paciente} />
+        <Stack.Screen name="Medico" component={Medico} />
+        <Stack.Screen name="Enfermeiro" component={Enfermeiro} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import {Button, Text, TextInput, View} from 'react-native';
-import Dropdown from 'react-native-input-select';
-export default function Login(): JSX.Element {
+import DropdownSelect from 'react-native-input-select';
+
+export default function Login({navigation}: any): JSX.Element {
   const [senha, setSenha] = useState('');
   const [user, setUser] = useState('');
   return (
     <View>
-      <Dropdown
+      <DropdownSelect
         label="Tipo de usuario"
         placeholder="Selecione o seu tipo de usuario"
         options={[
@@ -19,6 +20,21 @@ export default function Login(): JSX.Element {
         selectedValue={user}
         onValueChange={(value: React.SetStateAction<string>) => setUser(value)}
         primaryColor={'green'}
+        // eslint-disable-next-line react-native/no-inline-styles
+        labelStyle={{
+          color: 'teal',
+          fontSize: 15,
+          fontWeight: '500',
+          alignSelf: 'center',
+          marginTop: '20%',
+        }}
+        // eslint-disable-next-line react-native/no-inline-styles
+        dropdownContainerStyle={{
+          borderColor: 'red',
+          alignContent: 'center',
+        }}
+        modalBackgroundStyle={{backgroundColor: 'rgba(196, 198, 246, 0.5)'}}
+        checkboxStyle={{backgroundColor: 'red', borderRadius: 5}}
       />
       <Text>{user}</Text>
       <TextInput
@@ -29,7 +45,18 @@ export default function Login(): JSX.Element {
           setSenha(value);
         }}
       />
-      <Button title="Entrar" onPress={() => {}} />
+      <Button
+        title="Entrar"
+        onPress={() => {
+          if (user === 'ME') {
+            navigation.navigate('Medico');
+          } else if (user === 'EN') {
+            navigation.navigate('Enfermeiro');
+          } else if (user === 'PA') {
+            navigation.navigate('Paciente');
+          }
+        }}
+      />
     </View>
   );
 }
