1853f8a0b1806994bb1924d422bac5dda52cd274
Wesley <wesleycariutaba@gmail.com>
2023-04-15 13:48:46 -0300

    creating collection types

---
+++ backend/src/api/enfermeiro/routes/enfermeiro.ts
@@ -0,0 +1,7 @@
+/**
+ * enfermeiro router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::enfermeiro.enfermeiro');
---
+++ backend/src/api/enfermeiro/services/enfermeiro.ts
@@ -0,0 +1,7 @@
+/**
+ * enfermeiro service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::enfermeiro.enfermeiro');
