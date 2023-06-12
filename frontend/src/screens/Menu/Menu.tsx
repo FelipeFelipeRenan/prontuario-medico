52f440a22096870c38a3d365148715604f4a4e37
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-06-12 10:15:47 -0300

    fix: fixing stack

--- frontend/src/screens/Menu/Menu.tsx
+++ frontend/src/screens/Menu/Menu.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Button} from 'native-base';
+import {Button, Stack} from 'native-base';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -59,82 +59,84 @@ export default function Menu({navigation}: any): JSX.Element {
   return (
     <View>
       <Text style={styles.title}>Bem vindo ao menu</Text>
-      {/* Administrador */}
-      {user && user.accessLevel === 0 && (
-        <View style={styles.container}>
-          <Button
-            onPress={() => navigation.navigate('Cadastrar Médico')}
-            size={120}
-            borderRadius={4}>
-            <Text style={styles.cardText}> Cadastrar médico </Text>
-          </Button>
-          <Button
-            onPress={() => navigation.navigate('Cadastrar Enfermeira')}
-            size={120}
-            borderRadius={4}>
-            <Text style={styles.cardText}>Cadastrar enfermeira(o)</Text>
-          </Button>
-          <Button
-            onPress={() => navigation.navigate('Cadastrar Paciente')}
-            size={120}
-            borderRadius={4}>
-            <Text style={styles.cardText}>Cadastrar paciente</Text>
-          </Button>
-          <Button
-            onPress={() => navigation.navigate('Mostrar Users')}
-            size={120}
-            borderRadius={4}>
-            <Text style={styles.cardText}>Ver usuários do sistema</Text>
-          </Button>
-          {/* Logout */}
-          <Button size={120} borderRadius={4} onPress={() => logout()}>
-            Sair
-          </Button>
-        </View>
-      )}
-      {/* Médico e enfermeira*/}
-      {user && (user.accessLevel === 1 || user.accessLevel === 2) && (
-        <View style={styles.container}>
-          <Button
-            onPress={() => navigation.navigate('Cadastrar Consulta')}
-            size={120}
-            borderRadius={4}>
-            <Text style={styles.cardText}>Cadastrar consulta</Text>
-          </Button>
+      
+        
+        {/* Administrador */}
+        {user && user.accessLevel === 0 && (
+          <Stack direction="column" space={4} w="100%" alignItems="center" marginTop={30}>
+            <Button
+              onPress={() => navigation.navigate('Cadastrar Médico')}
+              size={120}
+              borderRadius={4}>
+              <Text style={styles.cardText}> Cadastrar médico </Text>
+            </Button>
+            <Button
+              onPress={() => navigation.navigate('Cadastrar Enfermeira')}
+              size={120}
+              borderRadius={4}>
+              <Text style={styles.cardText}>Cadastrar enfermeira(o)</Text>
+            </Button>
+            <Button
+              onPress={() => navigation.navigate('Cadastrar Paciente')}
+              size={120}
+              borderRadius={4}>
+              <Text style={styles.cardText}>Cadastrar paciente</Text>
+            </Button>
+            <Button
+              onPress={() => navigation.navigate('Mostrar Usuários')}
+              size={120}
+              borderRadius={4}>
+              <Text style={styles.cardText}>Ver usuários do sistema</Text>
+            </Button>
+            {/* Logout */}
+            <Button size={120} borderRadius={4} onPress={() => logout()}>
+              Sair
+            </Button>
+          </Stack>
+        )}
+        {/* Médico e enfermeira*/}
+        {user && (user.accessLevel === 1 || user.accessLevel === 2) && (
+          <Stack direction="column" space={4} w="100%" alignItems="center" marginTop={30}>
+            <Button
+              onPress={() => navigation.navigate('Cadastrar Consulta')}
+              size={120}
+              borderRadius={4}>
+              <Text style={styles.cardText}>Cadastrar consulta</Text>
+            </Button>
 
-          <Button
-            onPress={() => navigation.navigate('Mostrar Consultas')}
-            size={120}
-            borderRadius={4}>
-            <Text style={styles.cardText}>Ver consultas</Text>
-          </Button>
+            <Button
+              onPress={() => navigation.navigate('Mostrar Consultas')}
+              size={120}
+              borderRadius={4}>
+              <Text style={styles.cardText}>Ver consultas</Text>
+            </Button>
 
-          {/* Logout */}
-          <Button onPress={() => logout()} size={120} borderRadius={4}>
-            <Text style={styles.cardText}>Sair</Text>
-          </Button>
-        </View>
-      )}
-      {/*Paciente*/}
-      {user && user.accessLevel === 3 && (
-        <View style={styles.container}>
-          <Button
-            size={120}
-            borderRadius={4}
-            onPress={() => navigation.navigate('Mostrar Prontuario')}>
-            <Text style={styles.cardText}>Consultar prontuário</Text>
-          </Button>
+            {/* Logout */}
+            <Button onPress={() => logout()} size={120} borderRadius={4}>
+              <Text style={styles.cardText}>Sair</Text>
+            </Button>
+          </Stack>
+        )}
+        {/*Paciente*/}
+        {user && user.accessLevel === 3 && (
+          <Stack direction="column" space={4} w="100%" alignItems="center" marginTop={30}> 
+            <Button
+              size={120}
+              borderRadius={4}
+              onPress={() => navigation.navigate('Mostrar Prontuario')}>
+              <Text style={styles.cardText}>Consultar prontuário</Text>
+            </Button>
 
-          {/* Logout */}
-          <Button
-            size={120}
-            borderRadius={4}
-            onPress={() => logout()}
-            alignSelf="center">
-            <Text style={styles.cardText}>Sair</Text>
-          </Button>
-        </View>
-      )}
+            {/* Logout */}
+            <Button
+              size={120}
+              borderRadius={4}
+              onPress={() => logout()}
+              alignSelf="center">
+              <Text style={styles.cardText}>Sair</Text>
+            </Button>
+          </Stack>
+        )}
     </View>
   );
 }
